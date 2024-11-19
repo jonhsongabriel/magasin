363c97e8cad23f1edd984181caeb4c984be1b511
Jonhsongabriel <jonhsonratsimbaherison@gmail.com>
2024-11-19 11:54:14 +0300

    Page a propos

--- magasin-en-ligne/src/pages/Apropos.js
+++ magasin-en-ligne/src/pages/Apropos.js
@@ -2,7 +2,51 @@
 import React from 'react';
 
 const Apropos = () => {
-  return <h1>Bienvenue sur la page d'accueil</h1>;
+  return (
+    <div className=' container p-4 m-4'>
+          <h1 className='text-center text-success'>Bienvenue sur magasin en ligne </h1>
+          <h4 className='text-success'>A propos de notre magasin en ligne</h4> 
+          <p>
+            Bienvenue sur notre plateforme, votre destination 
+            incontournable pour une expérience d'achat moderne et pratique.
+             Nous sommes spécialisés dans la vente en ligne de produits variés, 
+             adaptés à tous vos besoins, tout en mettant l'accent sur la qualité, 
+             la simplicité et la satisfaction de nos clients.
+          </p>
+          <p>
+          <h4 className='text-success'>Notre mission</h4>
+          Notre objectif est de vous offrir une expérience d'achat en 
+          ligne rapide, fiable et agréable. Grâce à notre interface intuitive et à 
+          nos catégories organisées, vous pouvez facilement explorer et trouver ce 
+          que vous cherchez en quelques clics.
+
+          </p> 
+
+        <p>
+           <h4 className='text-success'>Pourquoi choisir notre magasin en ligne ?</h4>
+           Large choix de produits : Nous proposons une sélection diversifiée pour 
+           répondre à vos attentes.
+           Produits de qualité : Tous nos articles sont soigneusement sélectionnés pour 
+           garantir votre satisfaction.
+           Livraison rapide : Où que vous soyez, nous nous assurons que vos commandes
+            vous parviennent dans les meilleurs délais.
+            Service client dédié : Notre équipe est là pour répondre à 
+            toutes vos questions et résoudre vos préoccupations.
+         </p>
+         <p>   
+         <h4 className='text-success'>Nos engagements</h4>
+        Nous nous engageons à respecter des normes élevées de qualité et à offrir une 
+        transparence totale sur tous nos produits et services. La confiance de nos clients
+         est notre priorité, et nous nous efforçons chaque jour de la mériter.
+
+         Nous vous remercions de votre confiance et sommes ravis de vous compter parmi nos 
+         visiteurs. Parcourez notre boutique dès aujourd'hui et découvrez une nouvelle manière de
+          faire vos achats !
+
+        </p>
+
+</div>
+        );
 };
 
 export default Apropos;
